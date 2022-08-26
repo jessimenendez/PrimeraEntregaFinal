f1f9e612c6ac7508e9a6b93d929de269d613ea55
Jessica Menendez <jessica.menendez@claro.com.ar>
2022-08-26 15:55:25 -0300

    se agrega funcionalidad para editar y elominar

--- Universidad/Academico/urls.py
+++ Universidad/Academico/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('registrarCurso/', views.registrarCurso)
+    path('registrarCurso/', views.registrarCurso),
+    path('edicionCurso/<codigo>', views.edicionCurso),
+    path('editarCurso/', views.editarCurso),
+    path('eliminarCurso/<codigo>', views.eliminarCurso)
 ]
--- Universidad/Academico/views.py
+++ Universidad/Academico/views.py
@@ -12,3 +12,24 @@ def registrarCurso(request):
     curso = Curso.objects.create(codigo=codigo, nombre=nombre)
     
     return redirect('/')
+
+def edicionCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    return render(request, "edicionCurso.html", {"curso": curso})
+
+def editarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    
+    curso = Curso.objects.get(codigo=codigo)
+    curso.nombre = nombre
+    curso.save()
+    
+    return redirect('/')
+
+def eliminarCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    
+    return redirect('/')
+
