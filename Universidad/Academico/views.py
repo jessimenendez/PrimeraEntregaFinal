3be7e3317b9fc28861f42d4e851a1fafcc53a0fd
Jessica Menendez <jessica.menendez@claro.com.ar>
2022-08-26 15:14:00 -0300

    se agrega el registro de un curso

--- Universidad/Academico/views.py
+++ Universidad/Academico/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Curso
 # Create your views here.
 def home(request):
     cursoListado = Curso.objects.all()
     return render(request, "gestionCursos.html", {"cursos": cursoListado})
+
+def registrarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    
+    curso = Curso.objects.create(codigo=codigo, nombre=nombre)
+    
+    return redirect('/')
